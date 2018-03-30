44538a1edde50de57df53565903f5255f8364ec8
Alex Chi <alexchi@c4g.io>
2018-03-30 23:36:24 +0800

    fix the package error in Build.cs

--- libdraco_ue4.19.Build.cs
+++ libdraco_ue4.19.Build.cs
@@ -11,6 +11,8 @@ public class libdraco_ue4 : ModuleRules
         string DracoPath = System.IO.Path.Combine(ModuleDirectory, "libdraco-1.2.5");
         string IncludePath = System.IO.Path.Combine(DracoPath, "include");
         string LibPath = "";
+        string LibFilePath1 = "";
+        string LibFilePath2 = "";
 
         if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
         {
@@ -29,32 +31,34 @@ public class libdraco_ue4 : ModuleRules
 
             LibPath = System.IO.Path.Combine(DracoPath, "lib", PlatformName, VSName);
 
-            PublicAdditionalLibraries.Add("dracodec.lib");
-            PublicAdditionalLibraries.Add("dracoenc.lib");
+            LibFilePath1 = System.IO.Path.Combine(LibPath, "dracodec.lib");
+            LibFilePath2 = System.IO.Path.Combine(LibPath, "dracoenc.lib");
         }
         else if (Target.Platform == UnrealTargetPlatform.Linux)
         {
             LibPath = System.IO.Path.Combine(DracoPath, "lib", "linux");
 
-            PublicAdditionalLibraries.Add("libdracodec.a");
-            PublicAdditionalLibraries.Add("libdracoenc.a");
+            LibFilePath1 = System.IO.Path.Combine(LibPath, "libdracodec.a");
+            LibFilePath2 = System.IO.Path.Combine(LibPath, "libdracoenc.a");
         }
         else if (Target.Platform == UnrealTargetPlatform.Mac)
         {
             LibPath = System.IO.Path.Combine(DracoPath, "lib", "macos");
 
-            PublicAdditionalLibraries.Add("libdracodec.a");
-            PublicAdditionalLibraries.Add("libdracoenc.a");
+            LibFilePath1 = System.IO.Path.Combine(LibPath, "libdracodec.a");
+            LibFilePath2 = System.IO.Path.Combine(LibPath, "libdracoenc.a");
         }
         else if (Target.Platform == UnrealTargetPlatform.IOS)
         {
             LibPath = System.IO.Path.Combine(DracoPath, "lib", "ios");
 
-            PublicAdditionalLibraries.Add("libdracodec.a");
-            PublicAdditionalLibraries.Add("libdracoenc.a");
+            LibFilePath1 = System.IO.Path.Combine(LibPath, "libdracodec.a");
+            LibFilePath2 = System.IO.Path.Combine(LibPath, "libdracoenc.a");
         }
 
         PublicIncludePaths.Add(IncludePath);
         PublicLibraryPaths.Add(LibPath);
+        PublicAdditionalLibraries.Add(LibFilePath1);
+        PublicAdditionalLibraries.Add(LibFilePath2);
     }
 }
